6ced9539d57d3d86e98ba805d98272e77a9981fb
zikrilazuardi <mzikrimpw15@gmail.com>
2022-09-19 19:03:51 +0700

    Upload ulang

---
+++ application/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>latihan 1</title>
+</head>
+
+<body>
+    Halo kawan.. Yuk kita belajar erb programming..!!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan method penjumlahan yaitu <?=$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
