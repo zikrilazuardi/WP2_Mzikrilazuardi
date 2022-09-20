42f6c953a1e28765941a18c0da956133f4c6a438
zikrilazuardi <mzikrimpw15@gmail.com>
2022-09-20 12:19:26 +0700

    Firts commit

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>" ;
-        echo"Nama saya zikri lazuardi
-             saya tinggal di daerah mempawah
+        echo"Nama saya yahya maulana
+             saya tinggal di daerah siantan
              olah raga yang saya sukai adalah
-             futsal"; 
+             renang"; 
     }
 }
