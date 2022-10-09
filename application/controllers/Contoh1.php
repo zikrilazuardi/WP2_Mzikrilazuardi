b70b86fb21ef60c9727c519e7f9a8690910c114e
zikrilazuardi <maulanayahya624@gmail.com>
2022-10-09 22:41:25 +0700

    new commit

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,7 +4,7 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>" ;
-        echo"Nama saya Muhammad zikri lazuardi
+        echo"Nama saya M zikri lazuardi
              saya tinggal di daerah mempawah
              olah raga yang saya sukai adalah
              futsal"; 
